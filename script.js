5ea74d397bacccb48c8baf92a0bb3e9a7aa40f2f
atrbyg24 <artwan520@gmail.com>
2024-12-19 14:14:57 -0500

    Write function for simulating computer choice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+// Function to simulate random choice by computer
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random()*2)
+    if (computerChoice == 0) {
+        return "rock"
+    }
+    else if (computerChoice == 1) {
+        return "paper"
+    }
+    else {
+        return "scissors"
+    }
+}
